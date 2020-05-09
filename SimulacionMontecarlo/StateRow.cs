c157d74126b610fae74ae8545b93eecf467dfe72
Isaias Avalle <isaiasavalle9@gmail.com>
2020-05-08 21:03:13 -0300

    Se agregan clases terminadas Simulator y StateRow

---
+++ SimulacionMontecarlo/StateRow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacionMontecarlo
+{
+    class StateRow
+    {
+        public int iterationNum { get; set; }
+        public double rndNumber { get; set; }
+        public int currentPassengers { get; set; }
+        public int deniedSeats { get; set; }
+        public int totalEarnings { get; set; }
+        public int extraPassengersCost { get; set; }
+        public int totalProfit { get; set; }
+        public int acumProfit { get; set; }
+
+    }
+}
